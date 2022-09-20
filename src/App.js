69a17a774120892813900230107aa606dd173f00
alpha2207_dev <ayan787ali.mev@gmail.com>
2022-09-20 09:48:11 +0530

    Added all crud operation successfully

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { useState } from "react";
 import Addtodo from "./components/Addtodo";
 import GetTodo from "./components/GetTodo";
 import Title from "./components/Title";
+import { db } from "./Firebase";
 
 function App() {
+
+  let [isUpdating, setisUpdating] = useState(false);
+  const handleDelete = async (id) => {
+    await deleteDoc(doc(db, 'todo', id));
+  }
+
+  const handleUpdate = async (title, id) => {
+    if(title === '' || title) {
+      alert("Title must have a value");
+    } else {
+      console.log(title === '');
+      await updateDoc(doc(db, 'todo', id), {
+        title
+      });
+    }
+
+  }
+
   return (
     <div className="App">
       <Title />
       <Addtodo />
-      <GetTodo/>
+      <GetTodo
+        handleDelete={handleDelete}
+        handleUpdate={handleUpdate}
+        isUpdating
+      />
     </div>
   );
 }
--- src/components/GetTodo.js
+++ src/components/GetTodo.js
@@ -2,8 +2,10 @@ import { getDocs, collection, query, onSnapshot, deleteDoc, doc, updateDoc } fro
 import React, { useEffect, useState } from 'react';
 import { db } from '../Firebase';
 
-export default function GetTodo() {
+export default function GetTodo({ handleDelete, handleUpdate }) {
     let [docs, setDocs] = useState([]);
+    let [isDisabled, setIsDisabled] = useState(true);
+    let [updatedTitle, setUpdatedTitle] = useState(null);
     useEffect(() => {
         const unsub = onSnapshot(query(collection(db, "todo")), (doc) => {
             doc.forEach(e => {
@@ -15,29 +17,22 @@ export default function GetTodo() {
         return () => unsub();
     }, []);
 
-    const handleDelete = async (id) => {
-        await deleteDoc(doc(db,'todo',id));
-    }
-
-    const handleUpdate = async (id) => {
-        await updateDoc(doc(db,'todo',id),{
-            title:"Hola Ho"
-        });
-    }
-
     return (
         <div>
             {docs.map(e => {
                 return (
                     <div>
-                        <p
+                        {updatedTitle}
+                        <input
                             key={e.id}
                             style={{ textDecoration: e.info.completed ? "line-through" : '' }}
-                        >
-                            {e.info.title}
-                        </p>
+                            value={updatedTitle === null ? e.info.title : updatedTitle}
+                            disabled={isDisabled}
+                            onChange={e => setUpdatedTitle(e.target.value)}
+                        />
+                        {!isDisabled && <button onClick={() => handleUpdate(updatedTitle, e.id)}>Confirm Update</button>}
                         <button onClick={() => handleDelete(e.id)}>Delete</button>
-                        <button onClick={()=>handleUpdate(e.id)}>Update</button>
+                        <button onClick={() => setIsDisabled(false)}>Update</button>
                     </div>)
             })}
         </div>
